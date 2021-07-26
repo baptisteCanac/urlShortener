62ef9ba18a9702495c4a4a7fa5e6934dea3ccd38
Baptiste <74823118+baptisteCanac@users.noreply.github.com>
2021-07-26 16:47:28 +0200

    Add files via upload
    début du code

---
+++ main.py
@@ -0,0 +1,10 @@
+# baptisteCanac
+import urllib.request
+
+def tiny_url(url):
+    apiurl = "https://tinyurl.com/api-create.php?url="
+    tinyurl = urllib.request.urlopen(apiurl + url).read()
+    return tinyurl.decode("utf-8")
+
+url = input("Enter your url to shorten : ")
+print(tiny_url(url))
